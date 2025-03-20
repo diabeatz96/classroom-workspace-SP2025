95c34903510f5c55c28fc3a518171f52a8f7ffdd
Adam Kostandy <75499553+diabeatz96@users.noreply.github.com>
2025-03-20 22:11:23 +0000

    Remove commented-out instructions for player ranking C++ program

--- Test Review/Jaimee.cpp
+++ Test Review/Jaimee.cpp
@@ -1,21 +1,4 @@
 /*
-
-7) 
-Write a full C++ program, including comments.
-
-A video game awards a player a rank based on their score.
-
-Write code that asks the user for their score. (Be sure to declare variables as needed.)
-
-· If the score is 90 or higher
-o print "Rank: Master"
-· If the score is 60 to 89,
-o print "Rank: Intermediate" AND
-o add 5 to score.
-· If the score is below 60
-o print "Rank: Beginner"
-
-
 */
 #include <stdio.h>
 #include <iostream>
